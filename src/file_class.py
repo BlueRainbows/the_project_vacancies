7c1482e90751c95fe8a37d4feee3266082acb0b9
hirrumi <BlueberryRainbows@yandex.ru>
2023-12-08 22:55:19 +0300

    Создан классы для фильтрации вакансий из платформ hh и sj

--- src/file_class.py
+++ src/file_class.py
@@ -193,3 +193,196 @@ class JSONSaver:
     def del_data(self):
         pass
 
+
+class FilterVacancy(ABC):
+
+    def __init__(self, search_query, top_n, filter_words):
+        self.vacancy = None
+        self.search_query = search_query.lower()
+        self.top_n = top_n
+        self.filter_words = filter_words
+
+    @abstractmethod
+    def open_json(self):
+        pass
+
+    @abstractmethod
+    def filter_vacancy_name(self):
+        pass
+
+    @abstractmethod
+    def filter_top_salary(self):
+        pass
+
+    @abstractmethod
+    def filter_the_key_words(self):
+        pass
+
+
+class FilterHH(FilterVacancy, ABC):
+    def __init__(self, search_query, top_n, filter_words):
+        super().__init__(search_query, top_n, filter_words)
+        self.vacancy = self.open_json()['hh_vacancy']
+
+    def open_json(self):
+        with open('./src/vacancy_file.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+
+    def filter_vacancy_name(self):
+        list_string_profession = []
+        for i in range(len(self.vacancy)):
+            if self.search_query in self.vacancy[i]['name_vacancy'].lower():
+                string_profession = self.vacancy[i]['name_vacancy'] + '\n' + \
+                                    self.vacancy[i]['salary_vacancy'] + '\n' + \
+                                    self.vacancy[i]['address_vacancy'] + '\n' + \
+                                    self.vacancy[i]['url_vacancy'] + '\n' + \
+                                    self.vacancy[i]['requirement_vacancy'] + '\n' + \
+                                    self.vacancy[i]['schedule_vacancy'] + '\n' + '\n'
+                list_string_profession.append(string_profession)
+        return list_string_profession
+
+    def filter_top_salary(self):
+        salary_full = []
+        max_salary = []
+        salary_string = ''
+        for i in range(len(self.vacancy)):
+            if 'От' and 'до' in self.vacancy[i]['salary_vacancy']:
+                splitting = self.vacancy[i]['salary_vacancy'].split(' ')
+                max_salary.append(int(splitting[-1]))
+            elif 'До' in self.vacancy[i]['salary_vacancy']:
+                max_salary.append(int(self.vacancy[i]['salary_vacancy'][3:]))
+            elif 'От' in self.vacancy[i]['salary_vacancy']:
+                max_salary.append(int(self.vacancy[i]['salary_vacancy'][3:]))
+        set_max_salary = set(max_salary)
+        max_salary.clear()
+        for sets in set_max_salary:
+            max_salary.append(sets)
+        max_salary.sort(reverse=True)
+        for salary in max_salary:
+            for i in range(len(self.vacancy)):
+                if self.vacancy[i]['salary_vacancy'] == 'Зарплата не указана':
+                    continue
+                splitting = self.vacancy[i]['salary_vacancy'].split(' ')
+                if len(splitting) != 2:
+                    if str(salary) == splitting[-1]:
+                        salary_string += self.vacancy[i]['name_vacancy'] + '\n' + \
+                                         self.vacancy[i]['salary_vacancy'] + '\n' + \
+                                         self.vacancy[i]['url_vacancy'] + '\n'
+                        salary_full.append(salary_string)
+                else:
+                    if str(salary) == splitting[-1]:
+                        salary_string += self.vacancy[i]['name_vacancy'] + '\n' + \
+                                         self.vacancy[i]['salary_vacancy'] + '\n' + \
+                                         self.vacancy[i]['url_vacancy'] + '\n'
+                        salary_full.append(salary_string)
+        return salary_full
+
+    def __len__(self):
+        return len(self.filter_top_salary())
+
+    def filter_the_key_words(self):
+        list_the_key_words = []
+        for i in range(len(self.vacancy)):
+            splitting_words = self.filter_words.split(',')
+            for spl in splitting_words:
+                if spl.lower() in self.vacancy[i]['professional_roles'].lower():
+                    string_profession = self.vacancy[i]['name_vacancy'] + '\n' + \
+                                        self.vacancy[i]['salary_vacancy'] + '\n' + \
+                                        self.vacancy[i]['address_vacancy'] + '\n' + \
+                                        self.vacancy[i]['url_vacancy'] + '\n' + \
+                                        self.vacancy[i]['requirement_vacancy'] + '\n' + \
+                                        self.vacancy[i]['schedule_vacancy'] + '\n' + '\n'
+                    list_the_key_words.append(string_profession)
+        return list_the_key_words
+
+
+class FilterSJ(FilterVacancy, ABC):
+    def __init__(self, search_query, top_n, filter_words):
+        super().__init__(search_query, top_n, filter_words)
+        self.vacancy = self.open_json()['sj_vacancy']
+
+    def open_json(self):
+        with open('./src/vacancy_file.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+
+    def filter_vacancy_name(self):
+        list_string_profession = []
+        for i in range(len(self.vacancy)):
+            if self.search_query in self.vacancy[i]['name_vacancy'].lower():
+                string_profession = self.vacancy[i]['name_vacancy'] + '\n' + \
+                                    self.vacancy[i]['salary_vacancy'] + '\n' + \
+                                    self.vacancy[i]['address_vacancy'] + '\n' + \
+                                    self.vacancy[i]['url_vacancy'] + '\n' + \
+                                    self.vacancy[i]['requirement_vacancy'][:100] + '...' + '\n' + \
+                                    self.vacancy[i]['schedule_vacancy'] + '\n' + '\n'
+                list_string_profession.append(string_profession)
+        return list_string_profession
+
+    def filter_top_salary(self):
+        salary_full = []
+        max_salary = []
+        salary_string = ''
+        for i in range(len(self.vacancy)):
+            if 'От' and 'до' in self.vacancy[i]['salary_vacancy']:
+                splitting = self.vacancy[i]['salary_vacancy'].split(' ')
+                max_salary.append(int(splitting[-1]))
+            elif 'До' in self.vacancy[i]['salary_vacancy']:
+                max_salary.append(int(self.vacancy[i]['salary_vacancy'][3:]))
+            elif 'От' in self.vacancy[i]['salary_vacancy']:
+                max_salary.append(int(self.vacancy[i]['salary_vacancy'][3:]))
+        set_max_salary = set(max_salary)
+        max_salary.clear()
+        for sets in set_max_salary:
+            max_salary.append(sets)
+        max_salary.sort(reverse=True)
+        for salary in max_salary:
+            for i in range(len(self.vacancy)):
+                if self.vacancy[i]['salary_vacancy'] == 'Зарплата не указана':
+                    continue
+                splitting = self.vacancy[i]['salary_vacancy'].split(' ')
+                if len(splitting) != 2:
+                    if str(salary) == splitting[-1]:
+                        salary_string += self.vacancy[i]['name_vacancy'] + '\n' + \
+                                         self.vacancy[i]['salary_vacancy'] + '\n' + \
+                                         self.vacancy[i]['url_vacancy'] + '\n'
+                        salary_full.append(salary_string)
+                else:
+                    if str(salary) == splitting[-1]:
+                        salary_string += self.vacancy[i]['name_vacancy'] + '\n' + \
+                                         self.vacancy[i]['salary_vacancy'] + '\n' + \
+                                         self.vacancy[i]['url_vacancy'] + '\n'
+                        salary_full.append(salary_string)
+        return salary_full
+
+    def __len__(self):
+        return len(self.filter_top_salary())
+
+    def filter_the_key_words(self):
+        list_the_key_words = []
+        splitting_words = self.filter_words.split(',')
+        for spl in splitting_words:
+            for i in range(len(self.vacancy)):
+                for professional in self.vacancy[i]['professional_roles']:
+                    prof_title = professional['title'].lower()
+                    if spl.lower() in prof_title:
+                        string_profession = self.vacancy[i]['name_vacancy'] + '\n' + \
+                                            self.vacancy[i]['salary_vacancy'] + '\n' + \
+                                            self.vacancy[i]['address_vacancy'] + '\n' + \
+                                            self.vacancy[i]['url_vacancy'] + '\n' + \
+                                            self.vacancy[i]['requirement_vacancy'][:100] + '...' + '\n' + \
+                                            self.vacancy[i]['schedule_vacancy'] + '\n' + '\n'
+                        list_the_key_words.append(string_profession)
+                    prof_positions = professional['positions']
+                    for prof in prof_positions:
+                        professional_titles = prof['title'].lower()
+                        if spl.lower() in professional_titles:
+                            string_profession = self.vacancy[i]['name_vacancy'] + '\n' + \
+                                                self.vacancy[i]['salary_vacancy'] + '\n' + \
+                                                self.vacancy[i]['address_vacancy'] + '\n' + \
+                                                self.vacancy[i]['url_vacancy'] + '\n' + \
+                                                self.vacancy[i]['requirement_vacancy'][:100] + '...' + '\n' + \
+                                                self.vacancy[i]['schedule_vacancy'] + '\n' + '\n'
+                            list_the_key_words.append(string_profession)
+        return list_the_key_words
