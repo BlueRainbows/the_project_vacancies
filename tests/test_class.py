7cdfa3193353c73820aa0ff845c211421022f381
hirrumi <BlueberryRainbows@yandex.ru>
2023-12-10 17:46:20 +0300

    Создан пакет зависимостей и добавлены тесты

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,7 +1,6 @@
 import pytest
 import requests
-
-from src.file_class import HeadHunter, SuperJob
+from src.file_class import HeadHunter, SuperJob, VacancyHH, VacancySJ
 
 
 def test_HeadHunter():
@@ -20,3 +19,37 @@ def test_SuperJob():
     assert type(sj.get_api()) == dict
     sj.url = 'api.superjob.ru'
     assert sj.get_api() is None
+
+
+def test_VacancyHH():
+    """ Тест выполняет проверку правильности возвращаемого объекта,
+    обрабатывает исключения и не предвиденные ошибки в документе """
+    hh = HeadHunter()
+    hh_vacancy = VacancyHH(hh.get_api())
+    assert type(hh_vacancy.get_vacancy()) == list
+    with pytest.raises(Exception):
+        hh.url = 'api.hh.ru'
+        hh_vacancy = VacancyHH(hh.get_api())
+        hh_vacancy.get_vacancy()
+    hh.url = 'https://api.hh.ru/vacancies'
+    dels_value = hh.get_api()
+    del dels_value['items'][0]['name']
+    hh_vacancy = VacancyHH(dels_value)
+    assert hh_vacancy.name_vacancy is None
+
+
+def test_VacancySJ():
+    """ Тест выполняет проверку правильности возвращаемого объекта,
+    обрабатывает исключения и не предвиденные ошибки в документе """
+    sj = SuperJob()
+    sj_vacancy = VacancySJ(sj.get_api())
+    assert type(sj_vacancy.get_vacancy()) == list
+    with pytest.raises(Exception):
+        sj.url = 'api.superjob.ru'
+        sj_vacancy = VacancySJ(sj.get_api())
+        sj_vacancy.get_vacancy()
+    sj.url = 'https://api.hh.ru/vacancies'
+    dels_value = sj.get_api()
+    del dels_value['items'][0]['name']
+    sj_vacancy = VacancySJ(dels_value)
+    assert sj_vacancy.name_vacancy == None
